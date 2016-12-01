2b63355bcc88c5931232143f72a3a5240fb074e3
lhuang58 <nick>
2016-11-30 23:03:39 -0600

    final evaluation, precision 95%, recall 95%, F1 95%

--- stage4/tuneRandomForest.py
+++ stage4/tuneRandomForest.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_fscore_support
 
 def ave(score):
 	sum = 0.00
@@ -14,83 +15,36 @@ def ave(score):
 I_data = pd.read_csv('I_data.csv', header=0)
 I_label = pd.read_csv('I_label.csv', header=0)
 
+J_data = pd.read_csv('J_data.csv', header=0)
+J_label = pd.read_csv('J_label.csv', header=0)
+
+print(I_data.columns[12])
+print(I_data.columns[30])
+I_data.drop(I_data.columns[12], axis=1, inplace=True)
+I_data.drop(I_data.columns[30], axis=1, inplace=True)
+
+J_data.drop(I_data.columns[12], axis=1, inplace=True)
+J_data.drop(I_data.columns[30], axis=1, inplace=True)
+
 I_data = np.array(I_data)
+J_data = np.array(J_data)
+
 I_label = np.array(I_label['gold'])
+J_label = np.array(J_label['gold'])
 
 cv_num = 10
 
-# Random Forest
-'''
-rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-print("criterion = gini")
-print(scores)
-print(ave(scores))
-'''
-
-'''
-rfClf = RandomForestClassifier(n_estimators=100, criterion='entropy', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-print("criterion = entropy")
-print(scores)
-print(ave(scores))
-'''
-
-'''
-for i in [3,4]:
-	rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=i+1, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-	scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-	print("Max Depth: " + str(i+1))
-	print(scores)
-	print(ave(scores))
-'''
-
-'''
-max_score = 0
-max_index = 0
-for i in range(42):
-	_max_feature = i+1
-	rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=4, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=_max_feature, max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-	scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-	ave_score = ave(scores)
-	if(ave_score > max_score):
-		max_score = ave_score
-		max_index = i+1
-		print(max_score)
-		print(max_index)
-'''
-# max number of features: 6
-
-'''
-max_score = 0
-max_index = 0
-for i in range(2,20):
-	rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=4, min_samples_split=i, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=6, max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-	scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-	ave_score = ave(scores)
-	if(ave_score > max_score):
-		max_score = ave_score
-		max_index = i
-		print(max_score)
-		print(max_index)
-'''
-# min_sample_split = 3
-'''
-max_score = 0
-max_index = 0
-for i in range(1,20):
-    rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=4, min_samples_split=3, min_samples_leaf=i, min_weight_fraction_leaf=0.0, max_features=6, max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, verbose=0, warm_start=False, class_weight=None)
-    scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
-    ave_score = ave(scores)
-    if(ave_score > max_score):
-        max_score = ave_score
-        max_index = i
-        print(max_score)
-        print(max_index)
-'''
 # min_samples_leaf = 2
 
 rfClf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=4, min_samples_split=3, min_samples_leaf=2, min_weight_fraction_leaf=0.0, max_features=6, max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, verbose=0, warm_start=False, class_weight=None)
+'''
 scores = cross_val_score(rfClf, I_data, I_label, cv=cv_num)
 ave_score = ave(scores)
 print(ave_score)
+'''
+
+# Test on table J
+rfClf.fit(I_data, I_label)
+results = rfClf.predict(J_data)
+scores = precision_recall_fscore_support(J_label, results, average='micro')
+print(scores)
